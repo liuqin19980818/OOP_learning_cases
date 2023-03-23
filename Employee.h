003e2d9e982e64d7ed0d5c407b44b741528c1338
liuqin19980818 <liuqin19980818@gmail.com>
2023-03-23 07:40:48 +0000

    修改了typo,并且增加了项目简介

---
+++ Employee.h
@@ -0,0 +1,12 @@
+//普通职工文件
+#pragma once
+#include<iostream>
+#include"worker.h"
+class Employee :public Worker
+{
+public:
+	Employee(int ID,string name,int DID);
+	virtual void showINfo();
+	virtual string getDepname();
+};
+
